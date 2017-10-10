f79dcf9b3ac917894ba036c8b377639fe842ab60
System <none>
2017-10-10 15:15:36 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.static('public'));
 app.get("/imagesearch/", function (request, response) {
   //https://bronze-soarer.glitch.me/imagesearch/lol%20cats?offset=10    lol%20cats?offset=10
   var offset=request.query.offset;
-  var searchTerm=request.url.replace("https://bronze-soarer.glitch.me/imagesearch/","");
+  var searchItem=request.url.replace("https://bronze-soarer.glitch.me/imagesearch/","");
+  searchItem=searchItem.replace("?offset="+offset,"");
   mongoClient.connect(dbUrl,function(err,db){
     if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
@@ -26,11 +27,27 @@ app.get("/imagesearch/", function (request, response) {
     console.log('Connection established to my', dbUrl);
     var collection=db.collection('images');
     if(collection!=null){
-      var query={snippet:searchTerm};
+      var query={snippet:searchItem};
       //Search for the array of marching images snippet
       collection.find(query).toArray(function(err,data){
         if(err) throw err;
         console.log("Found Images are:"+data);
+        if(data!=null)
+          {
+             if(data._id!=null)
+            {
+                console.log(" Images array found in DB.");
+                db.close();
+                var returnedData="data array"
+                    //data[offset];
+                response.send(returnedData);
+            }
+          }
+        else
+          {
+           console.log(" port was not found in DB.");
+           response.send("Sorry!, but we can't find the corresponding images for the "+searchItem+" you requested for");
+          }
       });
     }
     else{
